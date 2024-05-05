063bf3183ec9a24ad2c42834771cfaf81b6c9319
rakib-molla <rakibmail1997@gmail.com>
2024-05-05 18:02:01 +0600

    recent search history add

--- public/app.js
+++ public/app.js
@@ -24,6 +24,20 @@ window.onload = function(){
         // console.log(error);
         getWeatherData()
     })
+
+    axios.get('/api/history')
+        .then(({data})=>{
+            if(data?.data?.length > 0){
+                updateHistory(data?.data)
+            }else{
+                console.log('data nai');
+                historyElm.innerHTML = 'No History Found'
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+
     cityInput.addEventListener('keypress', function(e){
         if(e.key === 'Enter'){
             if(e.target.value){
@@ -46,7 +60,7 @@ function getWeatherData(city=DEFAULT_CITY, coords){
     
     axios.get(url)
         .then(({data})=>{
-            console.log(data);
+            // console.log(data);
             let weather = {
                 icon: data.weather[0].icon,
                 city: data.name,
@@ -73,4 +87,28 @@ function setWeather(weather){
     temp.innerHTML = weather.temp;
     pressure.innerHTML = weather.pressure;
     humidity.innerHTML = weather.humidity;
+}
+
+function updateHistory(history){
+
+    historyElm.innerHTML = '';
+    history = history.reverse();
+
+    history.forEach(h =>{
+
+        let tempHistory = masterHistory.cloneNode(true);
+        tempHistory.id = '';
+        tempHistory.getElementsByClassName('condition')[0].src = `${ICON_URL}${h.icon}.png`;
+        tempHistory.getElementsByClassName('city')[0].innerHTML = h.name;
+        tempHistory.getElementsByClassName('country')[0].innerHTML = h.country;
+        tempHistory.getElementsByClassName('main')[0].innerHTML = h.main;
+        tempHistory.getElementsByClassName('description')[0].innerHTML = h.description;
+        tempHistory.getElementsByClassName('temp')[0].innerHTML = h.temp;
+        tempHistory.getElementsByClassName('pressure')[0].innerHTML = h.pressure;
+        tempHistory.getElementsByClassName('humidity')[0].innerHTML = h.humidity;
+
+        historyElm.appendChild(tempHistory);
+    })
+
+    
 }
